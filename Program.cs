2e2a4d3678e13838a9022e83a4f19e3f0947fd05
MasterSmoke3 <112854234+MasterSmoke3@users.noreply.github.com>
2023-09-29 11:06:49 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class HighScores {
+constructor() {
+this.scores = [];
+this.maxScores = 10;
+}
+
+addScore(score) {
+if (this.scores.length <= this.maxScores) {
+this.scores.push(score);
+}
+}
+
+removeScore(score) {
+this.sctores.splice(index, 1);
+}
+}
+
+class Node {
+constructor(value) {
+this.value = value;
+this.next = next;
+}
+}
+
+class SinglyLinkedList {
+constructor(head) {
+this.head = head;
+this.length = 1;
+}
+}
+
+addElem(number) {
+const lastElem = this.head;
+for (let i = 0; i <= this.length; i++) {
+let lastElem = lastElem.next;
+}
+while (lastElem, next = null) { //не равно
+lastElem = lastElem.next
+}
+
+const newNode = new Node(number, null);
+lastElem next = newNode;
+}
+
+deleteElem(index) {
+let elemToDelete = head;
+let prevElem = null;
+for (int i = 0; i <= index; i++ ) {
+prevElem = elemToDelete;
+elemToDelete - elemToDelete.next;
+}
+prevElem next = elemToDelete.next;
+}
+
+const node1 = new Node(7, null);
+const node2 = new Node(3, node1);
+const list = new SinglyLinkedList(node2);
+
+// const obj2 = {value: 3};
+// const obj1 = {value: };
+
+// const test = new HighScores();
+
+node2.next
